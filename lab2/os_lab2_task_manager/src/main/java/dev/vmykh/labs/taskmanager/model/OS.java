973ccbefdec6d3cf9c10eb0abc0f8a116f59f3a4
Vitaly Mykhalko <mikhalko.ukr@gmail.com>
2015-05-14 06:14:36 +0300

    added full parsing of process by pid

--- lab2/os_lab2_task_manager/src/main/java/dev/vmykh/labs/taskmanager/model/OS.java
+++ lab2/os_lab2_task_manager/src/main/java/dev/vmykh/labs/taskmanager/model/OS.java
@@ -8,6 +8,7 @@ import java.util.List;
  */
 public class OS {
 //    private
+    private static String PROC_FOLDER = "/proc";
     private List<ProcessInfo> currentProcsInfo;
 
     public OS()
@@ -20,19 +21,58 @@ public class OS {
         ProcParser parser = new ProcParser();
         ProcessInfo pi = new ProcessInfo();
 
-
+        long totalCpuTimeStart = 0;
+        long totalCpuTimeFinish = 0;
+        ProcessParsedInfo ppiStart = null;
+        ProcessParsedInfo ppiFinish = null;
 
         try {
-            ProcessParsedInfo ppi = parser.getParsedProcessInfo(pid);
+            ppiStart = parser.getParsedProcessInfo(pid);
+            pi.setPid("" + ppiStart.getPid());
+            pi.setName(ppiStart.getName());
+            pi.setState(ppiStart.getState());
+            pi.setUsername(ppiStart.getUsername());
+            pi.setMemory("" + ppiStart.getMemory());
+            totalCpuTimeStart = parser.getCpuTotalTime();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            ppiFinish = parser.getParsedProcessInfo(pid);
+            totalCpuTimeFinish = parser.getCpuTotalTime();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        double processCpuPercentage = 100.0 * ((ppiFinish.getCtime() + ppiFinish.getUtime())
+                                    - (ppiStart.getCtime() + ppiStart.getUtime()))
+                                / (double)(totalCpuTimeFinish - totalCpuTimeStart);
+
+        pi.setCpu(String.format("%.2f", processCpuPercentage));
+
+        return pi;
+    }
+
+    private int[] getAllPids
+    {
         return null;
     }
 
     public List<ProcessInfo> getProcessesInfo()
     {
         return null;
+    }
+
+    public static void main(String[] args) {
+        OS os = new OS();
+        ProcessInfo pi = os.getProcessInfo(3160);
+        System.out.println(pi);
     }
 }
